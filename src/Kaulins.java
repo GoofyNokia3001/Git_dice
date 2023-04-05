4368e93af26c521b4158e97611abaecd905b699a
Klim <lvtskolnieksz@gmail.com>
2023-04-05 09:57:02 +0300

    Max value print

--- src/Kaulins.java
+++ src/Kaulins.java
@@ -3,12 +3,14 @@ import java.util.Scanner;
 
 public class Kaulins {
 	static void mestKaulinu(int reizes) {
-		int skaitlis;
+		int skaitlis, lielakais=0;
 		Random rand = new Random();
 		for(int i=1; i<=reizes; i++) {
 			skaitlis = rand.nextInt(6)+1;	
 			System.out.println("Uzkrita skaitlis "+skaitlis);
+			if(lielakais<skaitlis) {lielakais=skaitlis;}
 		}
+		System.out.println("Lielakais no skaitliem kas izkrita bija: "+lielakais);
 	}
 	
 	public static void main(String[] args) {
